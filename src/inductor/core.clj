f2b0828a2e7a5a1fb8a526948c8587f41d6cd0a4
David Conner <dconner.pro@gmail.com>
2013-07-21 14:15:39 -0600

    core: started implementing inductor.sysex

--- src/inductor/core.clj
+++ src/inductor/core.clj
@@ -1,6 +1,20 @@
 (ns inductor.core
   (:require gloss.core))
 
-(defcodec )
+(ns inductor.sysex)
+
+;; sample sysex msg
+;; sysex_msg = [0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7]
+
+(defcodec header (enum :byte { :hdr 0xf0 }))
+(defcodec vendor (enum :byte { :korg 0x72 )))
+
+;; (defn header?)
+
+;;
+;; (defcodec tag-byte-array
+;;   (ordered-map :tag-type    :TAG_Byte_Array
+;;                :payload (repeated :byte :prefix :int32-be)))
+
 
 
