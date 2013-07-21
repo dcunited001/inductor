3266a1a55c2249a55aa15cae5570f4f956cdea65
David Conner <dconner.pro@gmail.com>
2013-07-21 14:29:30 -0600

    defined basic codecs for describing sysex messages

--- src/inductor/core.clj
+++ src/inductor/core.clj
@@ -7,9 +7,23 @@
 ;; sysex_msg = [0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7]
 
 (defcodec header (enum :byte { :hdr 0xf0 }))
-(defcodec vendor (enum :byte { :korg 0x72 )))
+(defcodec vendor (enum :byte { :korg 0x42 )))
+(defcodec model (enum :byte { :microkorg 0x3g }))
+(defcodec device (enum :byte { :master 0x58 }))
+(defcodec req (enum :byte { :send 0x12
+                            :recv 0x11 }))
+;; (defcodec address) ;; the address of the setting 
+;; (defcodec param) ;; the parameter we're sending (only for req=0x12)
+;; (defcodec roland-checksum) 
 
-;; (defn header?)
+(defcodec footer (enum :byte { :ftr 0xf7 }))
+
+;; write macro to define sysex messages
+
+;; define a series of frames that defines a sysex commands
+;; (defcodec microkorg-current-program-datadump)
+
+;; define a series of frames that describes a sysex response
 
 ;;
 ;; (defcodec tag-byte-array
