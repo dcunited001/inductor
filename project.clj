dfa5b700146604bb274073b73c7a4ee6916b453a
David Conner <dconner.pro@gmail.com>
2013-07-21 10:51:03 -0600

    project.clj: updated with gloss/overtone

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject inductor "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone "0.8.1"]
+                 [gloss "0.2.2-rc1"]])
+
+;; [nippy "2.0.0-RC1"]
+;; [vertigo "0.1.0-SNAPSHOT"]
+;; [aleph "0.3.0-SNAPSHOT"]
+;; [clojurewerkz/serialism "1.1.0-SNAPSHOT"]
+
