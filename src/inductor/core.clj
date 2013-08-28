3a6aec498b20dac4df6a0cf0045ee0c51cc27103
David Conner <dconner.pro@gmail.com>
2013-08-28 14:21:46 -0600

    core: added example of a microkorg midi message

--- src/inductor/core.clj
+++ src/inductor/core.clj
@@ -2,12 +2,21 @@
 (ns inductor.sysex (:use gloss.core))
 
 ;; sample sysex msg
-;; sysex_msg = [0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7]
+;; e.g. sysex_msg = [0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7]
+;; e.g. korg_msg  = [0xF0, 0x42, 0x30, 0x58, 0x12, ... , 0xF7]
+
+;; microKORG format
+;; F0: exclusive status
+;; 42: Korg ID
+;; 3n: [n=0—F] MIDI channel
+;; 58: microKORG model ID (same as MS2000/MS2000R) ff: function ID (type of message)
+;; —
+;; F7: end of exclusive
 
 (defcodec hdr (enum :byte { :hdr 0xf0 }))
 (defcodec vendor (enum :byte { :korg 0x42 }))
-(defcodec model (enum :byte { :microkorg 0x3g }))
-(defcodec device (enum :byte { :master 0x58 }))
+(defcodec device (enum :byte { :1 0x30 :2 0x31 }))
+(defcodec model (enum :byte { :microkorg 0x58 }))
 
 ;; (defmacro system-exclusive-header)
 ;; (defmacro system-exclusive-footer)
@@ -28,7 +37,6 @@
 
 ;; define a series of frames that describes a sysex response
 
-;;
 ;; (defcodec tag-byte-array
 ;;   (ordered-map :tag-type    :TAG_Byte_Array
 ;;                :payload (repeated :byte :prefix :int32-be)))
