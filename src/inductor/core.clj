431302a6392b84008c8178c968e2b7523e8a5e8c
David Conner <dconner.pro@gmail.com>
2013-08-28 10:46:46 -0600

    core: updated require => use & changed codec names

--- src/inductor/core.clj
+++ src/inductor/core.clj
@@ -1,17 +1,20 @@
-(ns inductor.core
-  (:require gloss.core))
-
-(ns inductor.sysex)
+(ns inductor.core (:use gloss.core))
+(ns inductor.sysex (:use gloss.core))
 
 ;; sample sysex msg
 ;; sysex_msg = [0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7]
 
-(defcodec header (enum :byte { :hdr 0xf0 }))
-(defcodec vendor (enum :byte { :korg 0x42 )))
+(defcodec hdr (enum :byte { :hdr 0xf0 }))
+(defcodec vendor (enum :byte { :korg 0x42 }))
 (defcodec model (enum :byte { :microkorg 0x3g }))
 (defcodec device (enum :byte { :master 0x58 }))
-(defcodec req (enum :byte { :send 0x12
-                            :recv 0x11 }))
+
+;; (defmacro system-exclusive-header)
+;; (defmacro system-exclusive-footer)
+
+(defcodec req (enum :byte { :send 0x12 :recv 0x11 }))
+(defcodec address ())
+
 ;; (defcodec address) ;; the address of the setting 
 ;; (defcodec param) ;; the parameter we're sending (only for req=0x12)
 ;; (defcodec roland-checksum) 
@@ -30,5 +33,7 @@
 ;;   (ordered-map :tag-type    :TAG_Byte_Array
 ;;                :payload (repeated :byte :prefix :int32-be)))
 
-
+(defn get-send-codec [t]
+  "Gets the correct codec for a command"
+  )
 
